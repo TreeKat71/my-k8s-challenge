08f67eab22318298637d447b6a9fb32e246ef1f7
MullerH <muller79924@gmail.com>
2021-12-27 14:15:24 +0800

    Add error, visit endpoint

--- fastapi/main.py
+++ fastapi/main.py
@@ -1,8 +1,44 @@
+import sys
+import socket
+from typing import Optional
+
 from fastapi import FastAPI
+from pydantic import BaseModel
+from loguru import logger
+
+ERROR_DICT = {0: ImportError, 1: SyntaxError, 2: TypeError}
 
 app = FastAPI()
+fmt = '{time:YYYY-MM-DD HH:mm:ss.SSS}|{level}|{name}:{function}:{line}-{message}'
+# logger.add("/log/spam.log", level="DEBUG", format=fmt)
+logger.add(sys.stderr, level="ERROR", format=fmt)
+
+
+class Message(BaseModel):
+    from_: str
+    content: Optional[str] = None
 
 
 @app.get('/')
 async def hello_world():
     return "Hello World"
+
+
+@app.get('/error')
+async def error(num: int):
+    mod = num % 3
+    try:
+        raise ERROR_DICT[mod]
+    except Exception as e:
+        error_type = e.__class__.__name__
+        logger.error(error_type)
+        return {"type": error_type}
+
+
+@app.post('/leave_message/')
+async def leave_message(message: Message):
+    visitor = message.dict()['from_']
+    text = message.dict()['content'] or "Says Nothing"
+    node_name = socket.gethostname()
+    logger.info(f'{visitor} visited {node_name} left message: "{text}"')
+    return f'Hi {visitor}!, you visited {node_name}'
--- fastapi/test/test_main.py
+++ fastapi/test/test_main.py
@@ -1,3 +1,5 @@
+import socket
+
 from fastapi.testclient import TestClient
 
 from main import app
@@ -9,3 +11,23 @@ def test_hello_world():
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == "Hello World"
+
+
+def test_error():
+    response = client.get("/error?num=1")
+    assert response.status_code == 200
+    assert response.json() == {"type": "SyntaxError"}
+
+
+def test_leave_message():
+    visitor = "muller"
+    node_name = socket.gethostname()
+    response = client.post(
+        "/leave_message/",
+        json={
+            "from_": visitor,
+            "content": "DigitalOcean Kubernetes Challenge",
+        }
+    )
+    assert response.status_code == 200
+    assert response.json() == f'Hi {visitor}!, you visited {node_name}'
